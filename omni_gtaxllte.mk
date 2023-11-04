1295487b5329a6f737f2f38c8330fbd970f47d20
mrx7014 <safen894@gmail.com>
2023-11-04 13:13:38 +0000

    gtaxllte: initial twrp tree

---
+++ omni_gtaxllte.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from gtaxllte device
+$(call inherit-product, device/samsung/gtaxllte/device.mk)
+
+PRODUCT_DEVICE := gtaxllte
+PRODUCT_NAME := omni_gtaxllte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T585
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="gtaxlltexx-user 8.1.0 M1AJQ T585XXU7CUH1 release-keys"
+
+BUILD_FINGERPRINT := samsung/gtaxlltexx/gtaxllte:8.1.0/M1AJQ/T585XXU7CUH1:user/release-keys
